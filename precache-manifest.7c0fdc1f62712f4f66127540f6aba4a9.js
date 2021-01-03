dc12577f7d12ca1056da86c7dc44073199f9a696
Tiffany Guo <tiffguo@umich.edu>
2021-01-02 22:38:22 -0500

    Updates

---
+++ precache-manifest.7c0fdc1f62712f4f66127540f6aba4a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8ead51e20fdec5bdd562d7c49cbdff",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ba118dd8a406740d97c4",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba118dd8a406740d97c4",
+    "url": "/home/static/js/main.fba2f50a.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
